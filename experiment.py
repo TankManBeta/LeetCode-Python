b2dacbf7a7477fee706b5756df36acf0df5228e5
tankmanbeta <1148099884@qq.com>
2022-03-25 13:50:11 +0800

    modify experiment.py

--- experiment.py
+++ experiment.py
@@ -4,13 +4,3 @@
     @Author 坦克手贝塔
     @Date 2021/12/4 16:59
 """
-s = "barfoothefoobarman"
-words = ["foo", "bar"]
-len_words = len(words)
-len_word = len(words[0])
-len_window = len(words) * len(words[0])
-len_s = len(s)
-start = 0
-end = len_window - 1
-option_list = [s[start + len_word * i: start + len_word * (i + 1)] for i in range(0, len_words)]
-print(option_list)
