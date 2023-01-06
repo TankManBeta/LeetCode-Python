e2fb7a9be088c331d35856111f5e95a33060c386
tankmanbeta <1148099884@qq.com>
2023-01-06 14:35:28 +0800

    new

---
+++ problem1803_hard.py
@@ -0,0 +1,116 @@
+# -*- coding: utf-8 -*-
+
+"""
+    @Author 坦克手贝塔
+    @Date 2023/1/5 15:11
+"""
+from collections import Counter
+from typing import List
+
+"""
+给你一个整数数组 nums （下标 从 0 开始 计数）以及两个整数：low 和 high ，请返回 漂亮数对 的数目。
+漂亮数对 是一个形如 (i, j) 的数对，其中 0 <= i < j < nums.length 且 low <= (nums[i] XOR nums[j]) <= high 。
+
+示例 1：
+输入：nums = [1,4,2,7], low = 2, high = 6
+输出：6
+解释：所有漂亮数对 (i, j) 列出如下：
+    - (0, 1): nums[0] XOR nums[1] = 5 
+    - (0, 2): nums[0] XOR nums[2] = 3
+    - (0, 3): nums[0] XOR nums[3] = 6
+    - (1, 2): nums[1] XOR nums[2] = 6
+    - (1, 3): nums[1] XOR nums[3] = 3
+    - (2, 3): nums[2] XOR nums[3] = 5
+    
+示例 2：
+输入：nums = [9,8,4,2,1], low = 5, high = 14
+输出：8
+解释：所有漂亮数对 (i, j) 列出如下：
+​​​​​    - (0, 2): nums[0] XOR nums[2] = 13
+    - (0, 3): nums[0] XOR nums[3] = 11
+    - (0, 4): nums[0] XOR nums[4] = 8
+    - (1, 2): nums[1] XOR nums[2] = 12
+    - (1, 3): nums[1] XOR nums[3] = 10
+    - (1, 4): nums[1] XOR nums[4] = 9
+    - (2, 3): nums[2] XOR nums[3] = 6
+    - (2, 4): nums[2] XOR nums[4] = 5
+"""
+"""
+思路：
+（1）暴力超时
+（2）分区间，cnt[x]⋅cnt[y]=cnt[x]⋅cnt[x⊕t]
+"""
+
+
+# HIGH_BIT = 14
+
+# class TrieNode:
+#     def __init__(self):
+#         self.children = [None, None]
+#         self.sum = 0
+
+# class Trie:
+#     def __init__(self):
+#         self.root = TrieNode()
+
+#     def add(self, num: int) -> None:
+#         cur = self.root
+#         for k in range(HIGH_BIT, -1, -1):
+#             bit = (num >> k) & 1
+#             if not cur.children[bit]:
+#                 cur.children[bit] = TrieNode()
+#             cur = cur.children[bit]
+#             cur.sum += 1
+
+#     def get(self, num: int, x: int) -> int:
+#         res = 0
+#         cur = self.root
+#         for k in range(HIGH_BIT, -1, -1):
+#             bit = (num >> k) & 1
+#             if (x >> k) & 1:
+#                 if cur.children[bit]:
+#                     res += cur.children[bit].sum
+#                 if not cur.children[bit ^ 1]:
+#                     return res
+#                 cur = cur.children[bit ^ 1]
+#             else:
+#                 if not cur.children[bit]:
+#                     return res
+#                 cur = cur.children[bit]
+#         res += cur.sum
+#         return res
+
+# class Solution:
+#     def countPairs(self, nums: List[int], low: int, high: int) -> int:
+#         def f(nums: List[int], x: int) -> int:
+#             res = 0
+#             trie = Trie()
+#             for i in range(1, len(nums)):
+#                 trie.add(nums[i - 1])
+#                 res += trie.get(nums[i], x)
+#             return res
+#         return f(nums, high) - f(nums, low - 1)
+
+class Solution:
+    @staticmethod
+    def countPairs(nums: List[int], low: int, high: int) -> int:
+        # count = 0
+        # n = len(nums)
+        # for i in range(n):
+        #     for j in range(i+1, n):
+        #         if low <= nums[i] ^ nums[j] <= high:
+        #             count += 1
+        # return count
+
+        ans, cnt = 0, Counter(nums)
+        high += 1
+        while high:
+            nxt = Counter()
+            for x, c in cnt.items():
+                # high%2*cnt[(high-1)^x] 相当于 cnt[(high-1)^x] if high%2 else 0
+                ans += c * (high % 2 * cnt[(high - 1) ^ x] - low % 2 * cnt[(low - 1) ^ x])
+                nxt[x >> 1] += c
+            cnt = nxt
+            low >>= 1
+            high >>= 1
+        return ans // 2
