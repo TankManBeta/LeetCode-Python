388d69a62afff9cf0558cfcc7f68817db49a14f2
tankmanbeta <1148099884@qq.com>
2023-04-16 12:49:09 +0800

    new

--- problem1023_medium.py
+++ problem1023_medium.py
@@ -34,28 +34,49 @@ from typing import List
 "FooBarTest" 可以这样生成："Fo" + "o" + "Ba" + "r" + "T" + "est".
 """
 """
-思路：对于query来说，如果当前是大写字母，则必须匹配到pattern中的大写字母；如果当前是小写字母则能匹配就匹配，因为再下一次遇到
-大写字母之前，如果可以匹配成功肯定可以消掉，否则的话下一次遇到大写字母就会return False。
+思路：
+（1）匹配pattern，看最后pattern是否能到末尾。对于query来说，如果当前是大写字母，则必须匹配到pattern中的大写字母；如果当前是
+小写字母则能匹配就匹配，因为再下一次遇到大写字母之前，如果可以匹配成功肯定可以消掉，否则的话下一次遇到大写字母就会return False。
+（2）匹配query，看最后query能否到末尾。如果指针 i 和 j 指向的字符不同，并且 s[i] 为小写字母，则指针 i 循环向后移动一位。如果
+指针 i 已经到达字符串 s 的末尾，或者指针 i 和 j 指向的字符不同，则返回 false。否则，指针 i 和 j 同时向后移动一位。当指针 j 
+到达字符串 t 的末尾时，我们需要判断字符串 s 中剩余的字符是否都为小写字母，若是则返回 true，否则返回 false。
 """
 
 
 class Solution:
     @staticmethod
     def camelMatch(queries: List[str], pattern: str) -> List[bool]:
-        def match(word):
-            p = 0
-            for letter in word:
-                if letter.isupper():
-                    if p < len(pattern) and pattern[p] == letter:
-                        p += 1
-                    else:
-                        return False
-                else:
-                    if p < len(pattern) and pattern[p] == letter:
-                        p += 1
-            return p == len(pattern)
+        # n = len(pattern)
+        #
+        # def match(word):
+        #     p = 0
+        #     for letter in word:
+        #         if letter.isupper():
+        #             if p < n and pattern[p] == letter:
+        #                 p += 1
+        #             else:
+        #                 return False
+        #         else:
+        #             if p < n and pattern[p] == letter:
+        #                 p += 1
+        #     return p == n
+        #
+        # res = []
+        # for query in queries:
+        #     res.append(match(query))
+        # return res
 
-        res = []
-        for query in queries:
-            res.append(match(query))
-        return res
+        def check(s, t):
+            m, n = len(s), len(t)
+            i = j = 0
+            while j < n:
+                while i < m and s[i] != t[j] and s[i].islower():
+                    i += 1
+                if i == m or s[i] != t[j]:
+                    return False
+                i, j = i + 1, j + 1
+            while i < m and s[i].islower():
+                i += 1
+            return i == m
+
+        return [check(q, pattern) for q in queries]
